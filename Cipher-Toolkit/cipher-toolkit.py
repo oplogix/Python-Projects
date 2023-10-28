35a0c64b0066d79ab81309f0385b18d671697ade
OpLogix <tyus.ashworth@gmail.com>
2023-10-28 14:21:53 -0700

    Update cipher-toolkit.py

--- Cipher-Toolkit/cipher-toolkit.py
+++ Cipher-Toolkit/cipher-toolkit.py
@@ -16,32 +16,38 @@ print('''
  \   \ .'  '---'   `---'.|    |   ,/      |   | ,'  |   |.'                          ;   |,'     
   `---`              `---`    '---'       `----'    `---'                            '---'       ''')
 # Main menu
+import importlib
+
+# Define a dictionary to map user choices to module names
+cipher_modules = {
+    "1": "caesar_cipher",
+    "2": "rot13_cipher",
+    "3": "substitution_cipher",
+    "4": "atbash_cipher",
+}
+
+# Main menu
 while True:
     print("Choose an option:")
     print("1. Caesar Cipher")
     print("2. ROT-13")
     print("3. Substitution Cipher")
-    print("4. Exit")
+    print("4. Atbash Cipher")
+    print("5. Exit")
 
     choice = input("Enter your choice: ")
 
-    if choice == "1":
-        # Load the Caesar cipher module
-        cipher_module = importlib.import_module("ceasar_cipher")
-    elif choice == "2":
-        # Load the ROT-13 cipher module 
-        cipher_module = importlib.import_module("rot13_cipher")
-    elif choice == "3":
-        # Load the Substitution cipher module
-        cipher_module = importlib.import_module("substitution_cipher")
-    elif choice == "4":
+    if choice == "5":
         print("Goodbye!")
         break
+    elif choice in cipher_modules:
+        # Load the selected cipher module
+        module_name = cipher_modules[choice]
+        cipher_module = importlib.import_module(module_name)
+
+        # Get the user's choice (encrypt or decrypt)
+        #4user_choice = cipher_module.get_choice()
+
+        # Continue with the selected cipher (e.g., encoding/decoding) using functions from the loaded module
     else:
         print("Invalid choice. Please select a valid option.")
-        continue
-
-    # Get the user's choice (encrypt or decrypt)
-    user_choice = cipher_module.get_choice()
-
-    # Continue with the selected cipher (e.g., encoding/decoding) using functions from the loaded module
